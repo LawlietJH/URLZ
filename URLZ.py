55e5a1bf3d443e06f83df0ffcc9184d10f6abf8b
Lawliet <jerry_howl_lawliet@hotmail.com>
2019-07-24 01:35:50 -0500

    Update URLZ v1.0.1
    URLZ v1.0.1

--- URLZ.py
+++ URLZ.py
@@ -1,47 +1,33 @@
 
-
-def Hex_Asc(Hex):	#~ Hexadecimal a Ascii
-	
-	Hex = Hex.replace(' ', '')		#~ Quita los espacios.
-	
-	Ascii = ''.join((chr(int(Hex[i:i+2], 16)) for i in range(0, len(Hex), 2)))
-	
-	return Ascii
-
+rep = lambda r: r.replace(' ', '')
+hexToAsc = lambda h: ''.join((chr(int(rep(h)[i:i+2], 16)) for i in range(0, len(rep(h)), 2)))
 
 def getHexs(URL):
-	save = False
-	cont = 0
-	hexa = ''
 	salida = []
-	for c in URL:
-		if save:
-			hexa += c
-			if len(hexa) == 2:
-				if hexa not in salida:
-					salida.append(hexa)
-				save = False
-				cont = 0
-				hexa = ''
-		if '%' == c:
-			save = True
+	for i, c in enumerate(URL):
+		if not c == '%':
+			continue
+		else:
+			hexa = URL[i+1:i+3]
+			if hexa not in salida:
+				salida.append(hexa)
+	salida.sort()
 	return salida
-
 
 
 if __name__ == '__main__':
 	
 	l_hex = []
-	ver = '1.0.0'
-	# ~ URL_Prueba = 'http%3A%2F%2Fwww.mediafire.com%2Ffile%2F972uw7faxf6yswe%2FSeguridad%2BInformatica-20170725T135202Z-001.zip%3Ffbclid%3DIwAR1Eoa0-90t6rJZz8kZv2tkxOrcdOCQZapCKz4Due919ltKH_Z7F9ZFH-ZA'
+	ver = 'v1.0.1'
+	# ~ URL = 'http%3A%2F%2Fwww.mediafire.com%2Ffile%2F972uw7faxf6yswe%2FSeguridad%2BInformatica-20170725T135202Z-001.zip%3Ffbclid%3DIwAR1Eoa0-90t6rJZz8kZv2tkxOrcdOCQZapCKz4Due919ltKH_Z7F9ZFH-ZA'
 	
 	print('\n URLZ {}'.format(ver))
-	URL = input('\n\n [?] URL:\n ')
+	URL = input('\n [?] URL:\n ')
 	
 	hexs = getHexs(URL)
 	
 	for h in hexs:
-		asc = Hex_Asc(h)
+		asc = hexToAsc(h)
 		l_hex.append((h, asc))
 		URL = URL.replace('%'+h, asc)
 	
